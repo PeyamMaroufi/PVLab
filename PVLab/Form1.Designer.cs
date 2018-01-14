5c145bf0e627fa224135a2999ff59c753c27e12a
Peyam Larsson <Peyam.m@hotmail.com>
2018-01-14 17:31:01 +0100

    change properties

--- PVLab/Form1.Designer.cs
+++ PVLab/Form1.Designer.cs
@@ -29,31 +29,31 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.rbNone = new System.Windows.Forms.RadioButton();
-            this.rbRepeat = new System.Windows.Forms.RadioButton();
-            this.rbRapid = new System.Windows.Forms.RadioButton();
+            this.cbRes = new System.Windows.Forms.ComboBox();
+            this.cbCoupling = new System.Windows.Forms.ComboBox();
+            this.cbVoltage = new System.Windows.Forms.ComboBox();
+            this.cbStreaming = new System.Windows.Forms.ComboBox();
+            this.cbChannels = new System.Windows.Forms.ComboBox();
+            this.cbSamplingRate = new System.Windows.Forms.ComboBox();
+            this.cbTimebase = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
             this.rbETS = new System.Windows.Forms.RadioButton();
             this.rbSingle = new System.Windows.Forms.RadioButton();
             this.rbAuto = new System.Windows.Forms.RadioButton();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.cbSamplingsInt = new System.Windows.Forms.ComboBox();
-            this.cbTimebase = new System.Windows.Forms.ComboBox();
-            this.cbSamplingRate = new System.Windows.Forms.ComboBox();
-            this.cbChannels = new System.Windows.Forms.ComboBox();
-            this.cbStreaming = new System.Windows.Forms.ComboBox();
-            this.cbVoltage = new System.Windows.Forms.ComboBox();
-            this.cbCoupling = new System.Windows.Forms.ComboBox();
-            this.cbRes = new System.Windows.Forms.ComboBox();
+            this.rbRapid = new System.Windows.Forms.RadioButton();
+            this.rbRepeat = new System.Windows.Forms.RadioButton();
+            this.rbNone = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.txtStatus = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.txtSamplingInterval = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,6 +62,7 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtSamplingInterval);
             this.panel1.Controls.Add(this.cbRes);
             this.panel1.Controls.Add(this.cbCoupling);
             this.panel1.Controls.Add(this.cbVoltage);
@@ -69,7 +70,6 @@
             this.panel1.Controls.Add(this.cbChannels);
             this.panel1.Controls.Add(this.cbSamplingRate);
             this.panel1.Controls.Add(this.cbTimebase);
-            this.panel1.Controls.Add(this.cbSamplingsInt);
             this.panel1.Controls.Add(this.btnStart);
             this.panel1.Controls.Add(this.rbETS);
             this.panel1.Controls.Add(this.rbSingle);
@@ -90,110 +90,71 @@
             this.panel1.Size = new System.Drawing.Size(1018, 113);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // cbRes
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Sampling Int:";
+            this.cbRes.FormattingEnabled = true;
+            this.cbRes.Location = new System.Drawing.Point(908, 73);
+            this.cbRes.Name = "cbRes";
+            this.cbRes.Size = new System.Drawing.Size(95, 21);
+            this.cbRes.TabIndex = 24;
             // 
-            // label2
+            // cbCoupling
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Timebase:";
+            this.cbCoupling.FormattingEnabled = true;
+            this.cbCoupling.Location = new System.Drawing.Point(908, 30);
+            this.cbCoupling.Name = "cbCoupling";
+            this.cbCoupling.Size = new System.Drawing.Size(95, 21);
+            this.cbCoupling.TabIndex = 23;
             // 
-            // label3
+            // cbVoltage
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(848, 73);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Resolution:";
+            this.cbVoltage.FormattingEnabled = true;
+            this.cbVoltage.Location = new System.Drawing.Point(744, 70);
+            this.cbVoltage.Name = "cbVoltage";
+            this.cbVoltage.Size = new System.Drawing.Size(95, 21);
+            this.cbVoltage.TabIndex = 22;
             // 
-            // label5
+            // cbStreaming
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(495, 33);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Sampling Rate:";
+            this.cbStreaming.FormattingEnabled = true;
+            this.cbStreaming.Location = new System.Drawing.Point(744, 30);
+            this.cbStreaming.Name = "cbStreaming";
+            this.cbStreaming.Size = new System.Drawing.Size(95, 21);
+            this.cbStreaming.TabIndex = 21;
             // 
-            // label4
+            // cbChannels
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(495, 73);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Channels:";
+            this.cbChannels.FormattingEnabled = true;
+            this.cbChannels.Location = new System.Drawing.Point(580, 70);
+            this.cbChannels.Name = "cbChannels";
+            this.cbChannels.Size = new System.Drawing.Size(95, 21);
+            this.cbChannels.TabIndex = 20;
             // 
-            // label6
+            // cbSamplingRate
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(681, 33);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(57, 13);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Streaming:";
+            this.cbSamplingRate.FormattingEnabled = true;
+            this.cbSamplingRate.Location = new System.Drawing.Point(580, 30);
+            this.cbSamplingRate.Name = "cbSamplingRate";
+            this.cbSamplingRate.Size = new System.Drawing.Size(95, 21);
+            this.cbSamplingRate.TabIndex = 19;
             // 
-            // label7
+            // cbTimebase
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(681, 73);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 13);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "Voltage:";
+            this.cbTimebase.FormattingEnabled = true;
+            this.cbTimebase.Location = new System.Drawing.Point(87, 68);
+            this.cbTimebase.Name = "cbTimebase";
+            this.cbTimebase.Size = new System.Drawing.Size(121, 21);
+            this.cbTimebase.TabIndex = 18;
             // 
-            // label8
+            // btnStart
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(848, 33);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(51, 13);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "Coupling:";
-            // 
-            // rbNone
-            // 
-            this.rbNone.AutoSize = true;
-            this.rbNone.Location = new System.Drawing.Point(247, 29);
-            this.rbNone.Name = "rbNone";
-            this.rbNone.Size = new System.Drawing.Size(51, 17);
-            this.rbNone.TabIndex = 10;
-            this.rbNone.TabStop = true;
-            this.rbNone.Text = "None";
-            this.rbNone.UseVisualStyleBackColor = true;
-            // 
-            // rbRepeat
-            // 
-            this.rbRepeat.AutoSize = true;
-            this.rbRepeat.Location = new System.Drawing.Point(247, 52);
-            this.rbRepeat.Name = "rbRepeat";
-            this.rbRepeat.Size = new System.Drawing.Size(60, 17);
-            this.rbRepeat.TabIndex = 11;
-            this.rbRepeat.TabStop = true;
-            this.rbRepeat.Text = "Repeat";
-            this.rbRepeat.UseVisualStyleBackColor = true;
-            // 
-            // rbRapid
-            // 
-            this.rbRapid.AutoSize = true;
-            this.rbRapid.Location = new System.Drawing.Point(247, 75);
-            this.rbRapid.Name = "rbRapid";
-            this.rbRapid.Size = new System.Drawing.Size(53, 17);
-            this.rbRapid.TabIndex = 12;
-            this.rbRapid.TabStop = true;
-            this.rbRapid.Text = "Rapid";
-            this.rbRapid.UseVisualStyleBackColor = true;
+            this.btnStart.Location = new System.Drawing.Point(401, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 16;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // rbETS
             // 
@@ -228,78 +189,110 @@
             this.rbAuto.Text = "Auto";
             this.rbAuto.UseVisualStyleBackColor = true;
             // 
-            // btnStart
+            // rbRapid
             // 
-            this.btnStart.Location = new System.Drawing.Point(401, 49);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
-            this.btnStart.TabIndex = 16;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.rbRapid.AutoSize = true;
+            this.rbRapid.Location = new System.Drawing.Point(247, 75);
+            this.rbRapid.Name = "rbRapid";
+            this.rbRapid.Size = new System.Drawing.Size(53, 17);
+            this.rbRapid.TabIndex = 12;
+            this.rbRapid.TabStop = true;
+            this.rbRapid.Text = "Rapid";
+            this.rbRapid.UseVisualStyleBackColor = true;
             // 
-            // cbSamplingsInt
+            // rbRepeat
             // 
-            this.cbSamplingsInt.FormattingEnabled = true;
-            this.cbSamplingsInt.Location = new System.Drawing.Point(87, 30);
-            this.cbSamplingsInt.Name = "cbSamplingsInt";
-            this.cbSamplingsInt.Size = new System.Drawing.Size(121, 21);
-            this.cbSamplingsInt.TabIndex = 17;
+            this.rbRepeat.AutoSize = true;
+            this.rbRepeat.Location = new System.Drawing.Point(247, 52);
+            this.rbRepeat.Name = "rbRepeat";
+            this.rbRepeat.Size = new System.Drawing.Size(60, 17);
+            this.rbRepeat.TabIndex = 11;
+            this.rbRepeat.TabStop = true;
+            this.rbRepeat.Text = "Repeat";
+            this.rbRepeat.UseVisualStyleBackColor = true;
             // 
-            // cbTimebase
+            // rbNone
             // 
-            this.cbTimebase.FormattingEnabled = true;
-            this.cbTimebase.Location = new System.Drawing.Point(87, 68);
-            this.cbTimebase.Name = "cbTimebase";
-            this.cbTimebase.Size = new System.Drawing.Size(121, 21);
-            this.cbTimebase.TabIndex = 18;
+            this.rbNone.AutoSize = true;
+            this.rbNone.Location = new System.Drawing.Point(247, 29);
+            this.rbNone.Name = "rbNone";
+            this.rbNone.Size = new System.Drawing.Size(51, 17);
+            this.rbNone.TabIndex = 10;
+            this.rbNone.TabStop = true;
+            this.rbNone.Text = "None";
+            this.rbNone.UseVisualStyleBackColor = true;
             // 
-            // cbSamplingRate
+            // label8
             // 
-            this.cbSamplingRate.FormattingEnabled = true;
-            this.cbSamplingRate.Location = new System.Drawing.Point(580, 30);
-            this.cbSamplingRate.Name = "cbSamplingRate";
-            this.cbSamplingRate.Size = new System.Drawing.Size(95, 21);
-            this.cbSamplingRate.TabIndex = 19;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(848, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Coupling:";
             // 
-            // cbChannels
+            // label7
             // 
-            this.cbChannels.FormattingEnabled = true;
-            this.cbChannels.Location = new System.Drawing.Point(580, 70);
-            this.cbChannels.Name = "cbChannels";
-            this.cbChannels.Size = new System.Drawing.Size(95, 21);
-            this.cbChannels.TabIndex = 20;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(681, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Voltage:";
             // 
-            // cbStreaming
+            // label6
             // 
-            this.cbStreaming.FormattingEnabled = true;
-            this.cbStreaming.Location = new System.Drawing.Point(744, 30);
-            this.cbStreaming.Name = "cbStreaming";
-            this.cbStreaming.Size = new System.Drawing.Size(95, 21);
-            this.cbStreaming.TabIndex = 21;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(681, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Streaming:";
             // 
-            // cbVoltage
+            // label4
             // 
-            this.cbVoltage.FormattingEnabled = true;
-            this.cbVoltage.Location = new System.Drawing.Point(744, 70);
-            this.cbVoltage.Name = "cbVoltage";
-            this.cbVoltage.Size = new System.Drawing.Size(95, 21);
-            this.cbVoltage.TabIndex = 22;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(495, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Channels:";
             // 
-            // cbCoupling
+            // label5
             // 
-            this.cbCoupling.FormattingEnabled = true;
-            this.cbCoupling.Location = new System.Drawing.Point(908, 30);
-            this.cbCoupling.Name = "cbCoupling";
-            this.cbCoupling.Size = new System.Drawing.Size(95, 21);
-            this.cbCoupling.TabIndex = 23;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(495, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sampling Rate:";
             // 
-            // cbRes
+            // label3
             // 
-            this.cbRes.FormattingEnabled = true;
-            this.cbRes.Location = new System.Drawing.Point(908, 73);
-            this.cbRes.Name = "cbRes";
-            this.cbRes.Size = new System.Drawing.Size(95, 21);
-            this.cbRes.TabIndex = 24;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(848, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resolution:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Timebase:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sampling Int:";
             // 
             // txtStatus
             // 
@@ -317,6 +310,13 @@
             this.label9.Size = new System.Drawing.Size(40, 13);
             this.label9.TabIndex = 2;
             this.label9.Text = "Status:";
+            // 
+            // txtSamplingInterval
+            // 
+            this.txtSamplingInterval.Location = new System.Drawing.Point(88, 29);
+            this.txtSamplingInterval.Name = "txtSamplingInterval";
+            this.txtSamplingInterval.Size = new System.Drawing.Size(120, 20);
+            this.txtSamplingInterval.TabIndex = 25;
             // 
             // PVLab
             // 
@@ -356,7 +356,6 @@
         private System.Windows.Forms.RadioButton rbNone;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.ComboBox cbTimebase;
-        private System.Windows.Forms.ComboBox cbSamplingsInt;
         private System.Windows.Forms.ComboBox cbRes;
         private System.Windows.Forms.ComboBox cbCoupling;
         private System.Windows.Forms.ComboBox cbVoltage;
@@ -365,6 +364,7 @@
         private System.Windows.Forms.ComboBox cbSamplingRate;
         private System.Windows.Forms.TextBox txtStatus;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSamplingInterval;
     }
 }
 
--- PVLab/Form1.cs
+++ PVLab/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PS5000AImports;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,84 @@ namespace PVLab
 {
     public partial class PVLab : Form
     {
+        string s;
         public PVLab()
         {
             InitializeComponent();
+
+            // Initiate GUI
+            InitiateGUI();
+        }
+
+
+        #region Initiating the combox box and other things
+
+        /// <summary>
+        /// Here we are initiating the combox box with the Enums available in the mapp
+        /// </summary>
+        private void InitiateGUI()
+        {
+            // Set up voltage combo box
+            foreach (var range in Enum.GetValues(typeof(Imports.Range)))
+            {
+                cbVoltage.Items.Add(range);
+            }
+
+            // Set up res combo box
+            foreach (var res in Enum.GetValues(typeof(Imports.DeviceResolution)))
+            {
+                cbRes.Items.Add(res);
+            }
+
+            // Set up channel combo box
+            foreach (var channel in Enum.GetValues(typeof(Imports.Channel)))
+            {
+                cbChannels.Items.Add(channel);
+            }
+
+            // Set up time combo box
+            foreach (var time in Enum.GetValues(typeof(Imports.Channel)))
+            {
+                cbTimebase.Items.Add(time);
+            }
+
+            // Set  up coupling combo box
+            foreach (var coupling in Enum.GetValues(typeof(Imports.Coupling)))
+            {
+                cbCoupling.Items.Add(coupling);
+            }
+
+        }
+
+        #endregion
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Making a new instance of SetupStreaming which runs the drivers and methods
+            SetupStreaming streaming = new SetupStreaming()
+            {
+                _resolution = (Imports.DeviceResolution)cbRes.SelectedIndex,
+                _coupling = (Imports.Coupling)cbCoupling.SelectedIndex,
+                _voltageRange = (Imports.Range)cbVoltage.SelectedIndex,
+                reportedTimeUnits = (Imports.ReportedTimeUnits)cbTimebase.SelectedIndex,
+                _sampleIntervall = uint.Parse(txtSamplingInterval.Text),
+            };
+
+            // Litsens to the choices of user
+            uint streaminIndex = (uint)cbStreaming.SelectedIndex;
+            switch (streaminIndex)
+            {
+                case 0:
+                    streaming.CollectStreamingImmediate();
+                    break;
+                case 1:
+                    streaming.CollectStreamingTriggered();
+                    break;             
+            }
+
+            // Updating status textbox
+            s = streaming.s;
+            txtStatus.Text += s + Environment.NewLine;
         }
     }
 }
